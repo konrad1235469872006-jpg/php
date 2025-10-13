61edd6f45427bb0129e8f0c7a9ac9b9128f0cabe
konrad1235469872006 <konrad1235469872006@gmail.com>
2025-10-13 12:14:49 +0200

    13.10.2025

---
+++ wyrażenia regularne/wyszukiwanie_daty.php
@@ -0,0 +1,9 @@
+<?php //wyszkuwanie daty
+$wzorzec =
+'/([0123])?\d)+[-\.\/]+([01]?\d)+[-\.\/]+(\d{4})/';
+$tekst(preg_match($wzorzec,$tekst))
+echo "znaleziono date";
+else
+    echo"nie znaleziono daty"
+?>
+
